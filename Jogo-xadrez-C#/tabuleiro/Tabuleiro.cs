4158f6195c559f1f011e648177b8efff6a92a6d0
NataFB <natafbt@gmail.com>
2026-04-27 11:51:39 -0300

    Comecando a implementar o tabuleiro

---
+++ Jogo-xadrez-C#/tabuleiro/Tabuleiro.cs
@@ -0,0 +1,16 @@
+﻿namespace tabuleiro
+{
+    internal class Tabuleiro
+    {
+        public int Linhas { get; set; }
+        public int Colunas { get; set; }
+        private Peca[,] _Pecas;
+
+        public Tabuleiro(int linhas, int colunas)
+        {
+            Linhas = linhas;
+            Colunas = colunas;
+            _Pecas = new Peca[linhas, colunas];
+        }
+    }
+}
