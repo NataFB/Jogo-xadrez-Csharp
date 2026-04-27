1e29bb57fa25b5c7234c743b317c69691fbe5e6a
NataFB <natafbt@gmail.com>
2026-04-27 11:16:46 -0300

    Criando a classe posicao

--- Jogo-xadrez-C#/Program.cs
+++ Jogo-xadrez-C#/Program.cs
@@ -1,10 +1,14 @@
-﻿namespace JogoXadrez
+﻿using tabuleiro;
+
+namespace JogoXadrez
 {
     class Program
     {
         static void Main(string[] args)
         {
-
+            Posicao P;
+            P = new Posicao(3, 4);
+            Console.WriteLine("Posição: " + P);
         }
     }
 }
