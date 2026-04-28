fc3acb8b9007d87acc8acaab79e184fbee6a97f7
NataFB <natafbt@gmail.com>
2026-04-28 10:36:44 -0300

    Criando a classe PosicaoXadrez

--- Jogo-xadrez-C#/Program.cs
+++ Jogo-xadrez-C#/Program.cs
@@ -8,19 +8,9 @@ namespace JogoXadrez
     {
         static void Main(string[] args)
         {
-            try
-            {
-                Tabuleiro tab = new Tabuleiro(8, 8);
-                tab.colocarPeca(new Torre(Cor.Preta, tab), new Posicao(0, 0));
-                tab.colocarPeca(new Torre(Cor.Preta, tab), new Posicao(1, 3));
-                tab.colocarPeca(new Rei(Cor.Preta, tab), new Posicao(2, 4));
-
-                Tela.imprimirTabuleiro(tab);
-            }
-            catch (TabuleiroException ex)
-            {
-                Console.WriteLine(ex.Message);
-            }
+          PosicaoXadrez pos = new PosicaoXadrez('c', 7);
+            Console.WriteLine(pos);
+            Console.WriteLine(pos.ToPosicao());
         }
     }
 }
---
+++ Jogo-xadrez-C#/xadrez/PosicaoXadrez.cs
@@ -0,0 +1,26 @@
+﻿using tabuleiro;
+
+namespace xadrez
+{
+    internal class PosicaoXadrez
+    {
+        public char Coluna { get; set; }
+        public int Linha { get; set; }
+        public PosicaoXadrez(char coluna, int linha)
+        {
+            Coluna = coluna;
+            Linha = linha;
+        }
+
+        public Posicao ToPosicao()
+        {
+            //obs: o "a" tem um valor inteiro associado, igual o b e assim por diante. Então , para converter a letra da coluna em um número, basta subtrair o valor inteiro de "a" do valor inteiro da letra da coluna. Assim consigo aplicar na matriz do jogo.
+            return new Posicao(8 - Linha, Coluna - 'a');
+        }
+
+        public override string ToString()
+        {
+            return "" + Coluna + Linha;
+        }   
+    }
+}
